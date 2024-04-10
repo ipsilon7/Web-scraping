7319f87c80d66bcbe2528d7634614439d5fdfe0c
Ivan <ivanarielplate@gmail.com>
2024-04-09 21:36:54 -0300

    Comenzando refactoring

--- Scraping.py
+++ Scraping.py
@@ -24,28 +24,33 @@ import time
 
 ############################## INICIO ##############################
 # Cargamos el webdriver de selenium
-service = webdriver.ChromeService(executable_path=r"C:\Users\Ivan\Documents\CODING\Python\chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-driver.get("https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52/SCAT_ID=-1/SCA_ID=-1/m=0/BUS=/compugarden.aspx")
+def web_driver():
+    service = webdriver.ChromeService(executable_path=r"C:\Users\Ivan\Documents\CODING\Python\chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
+    driver.get("https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52/SCAT_ID=-1/SCA_ID=-1/m=0/BUS=/compugarden.aspx")
+    return driver
 
 ############################## AVERIGUANDO CUANTAS PAGINAS HAY EN ESTA CATEGORIA ##############################
-show_items = int(driver.find_element(By.ID, value="cant_a_mostrar").get_attribute("value")) # Cantidad de productos mostrados por pagina
-total_items_raw = driver.find_element(By.XPATH, value="/html/body/div[3]/div[1]/section/div/div[2]/div[2]/div[2]/div/p").text
-total_items_raw = ' '.join(total_items_raw.split()) # Se borran los espacios extras
-total_items = int(total_items_raw[7:9]) # Se corta la parte con la cantidad de productos y se transforma a entero
-total_pages = round(total_items / show_items)
-if total_pages < (total_items / show_items):
-  total_pages += 1
+def cant_de_paginas(driver):
+    show_items = int(driver.find_element(By.ID, value="cant_a_mostrar").get_attribute("value")) # Cantidad de productos mostrados por pagina
+    total_items_raw = driver.find_element(By.XPATH, value="/html/body/div[3]/div[1]/section/div/div[2]/div[2]/div[2]/div/p").text
+    total_items_raw = ' '.join(total_items_raw.split()) # Se borran los espacios extras
+    total_items = int(total_items_raw[7:9]) # Se corta la parte con la cantidad de productos y se transforma a entero
+    total_pages = round(total_items / show_items)
+    if total_pages < (total_items / show_items):
+        total_pages += 1
+    return total_pages
 
 ############################## BUCLE DE SCRAPEO ##############################
 page = 1
 while page < (total_pages + 1):
-    time.sleep(2)
-    URL_BASE = f"https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52;SCAT_ID=-1;SCA_ID=-1;m=0;BUS=;A_PAGENUMBER={page};/compugarden.aspx"
-    pedido_obtenido = requests.get(URL_BASE)
-    html_obtenido = pedido_obtenido.text
-    # Parseamos el HTML
-    soup = BeautifulSoup(html_obtenido,"html.parser")
+    def parsear_pagina():
+        time.sleep(2)
+        URL_BASE = f"https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52;SCAT_ID=-1;SCA_ID=-1;m=0;BUS=;A_PAGENUMBER={page};/compugarden.aspx"
+        pedido_obtenido = requests.get(URL_BASE)
+        html_obtenido = pedido_obtenido.text
+        soup = BeautifulSoup(html_obtenido,"html.parser") # Parseamos el HTML
+        return soup
     ############################## EXTRACCION DE DATOS NUEVOS ##############################
     # Obtener y extraer datos
     divs_products = soup.find_all('div', class_='product')
@@ -63,14 +68,20 @@ while page < (total_pages + 1):
         price = price.replace(',', '.')
         price = float(price)
         precios.append(price)
-    # Variable fecha
-    current_date = date.today()
-    formatted_date = current_date.strftime("%d-%m-%Y")
-    fecha = []
-    i = 0
-    while i < len(products):
-        fecha.append(formatted_date)
-        i+=1
+   
+
+    def fecha_actual() -> str:
+        current_date = date.today()
+        formatted_date = current_date.strftime("%d-%m-%Y")
+        return formatted_date
+    
+    def fecha_por_producto(formatted_date:str, products:list) -> list:
+        fecha = []
+        i = 0
+        while i < len(products):
+            fecha.append(formatted_date)
+            i+=1
+        return fecha
 
     ############################## GUARDAR DATOS EN DATAFRAME ##############################
     # Crear diccionario para el dataframe
