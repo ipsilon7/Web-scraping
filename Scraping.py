68a19c02550609ac6db89db5bc6e85cceac49d39
Ivan <ivanarielplate@gmail.com>
2024-04-16 23:13:18 -0300

    Refactoring 1.02

--- Scraping.py
+++ Scraping.py
@@ -13,7 +13,7 @@
 .FINALIZAR PROGRAMA
 
 """""""""""""""""""""""""""""""""""""""
-############################## IMPORTAR PAQUETES ##############################
+# IMPORTAR PAQUETES
 from bs4 import BeautifulSoup
 from datetime import date
 from selenium import webdriver
@@ -22,17 +22,15 @@ import pandas as pd
 import requests
 import time
 
-############################## INICIO ##############################
+# INICIO
 # Cargamos el webdriver de selenium
 
 
-def web_driver() -> None:
+def web_driver():
     service = webdriver.ChromeService(executable_path=r"C:\Users\Ivan\Documents\CODING\Python\chromedriver.exe")
     driver = webdriver.Chrome(service=service)
     driver.get("https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52/SCAT_ID=-1/SCA_ID=-1/m=0/BUS=/compugarden.aspx")
     return driver
-
-############################## AVERIGUANDO CUANTAS PAGINAS HAY EN ESTA CATEGORIA ##############################
 
 
 def cant_de_paginas(driver) -> int:
@@ -46,17 +44,18 @@ def cant_de_paginas(driver) -> int:
     """
     show_items = int(driver.find_element(By.ID, value="cant_a_mostrar").get_attribute("value"))  # Cantidad de productos mostrados por pagina
     total_items_raw = driver.find_element(By.XPATH, value="/html/body/div[3]/div[1]/section/div/div[2]/div[2]/div[2]/div/p").text
-    total_items_raw = ' '.join(total_items_raw.split()) # Se borran los espacios extras
-    total_items = int(total_items_raw[7:9]) # Se corta la parte con la cantidad de productos y se transforma a entero
+    total_items_raw = ' '.join(total_items_raw.split())  # Se borran los espacios extras
+    total_items = int(total_items_raw[7:9])  # Se corta la parte con la cantidad de productos y se transforma a entero
     total_pages = round(total_items / show_items)
     if total_pages < (total_items / show_items):
         total_pages += 1
     return total_pages
 
-############################## BUCLE DE SCRAPEO ##############################
-page = 1
-while page < (cant_de_paginas() + 1):
-    
+# BUCLE DE SCRAPEO
+
+
+while page <= total_pages:
+
     def respuesta_url(page: int):
         """Estado de la url
 
@@ -70,9 +69,9 @@ while page < (cant_de_paginas() + 1):
         base_url = f"https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52;SCAT_ID=-1;SCA_ID=-1;m=0;BUS=;A_PAGENUMBER={page};/compugarden.aspx"
         pedido_obtenido = requests.get(base_url)
         if pedido_obtenido.status_code != 200:
-            return print("Pagina caida")
+            return print("Pagina inaccesible")
         return pedido_obtenido
-    
+
     def parsear_pagina(pedido_obtenido):
         """Parseo de la url
 
@@ -85,8 +84,8 @@ while page < (cant_de_paginas() + 1):
         html_obtenido = pedido_obtenido.text
         soup = BeautifulSoup(html_obtenido, "html.parser")  # Parseamos el HTML
         return soup
-    
-    ############################## EXTRACCION DE DATOS NUEVOS ##############################
+
+    # EXTRACCION DE DATOS NUEVOS
     # Obtener y extraer datos
     def extraccion_de_datos(soup):
         divs_products = soup.find_all('div', class_='product')
@@ -105,10 +104,10 @@ while page < (cant_de_paginas() + 1):
     def lista_precios(divs_precios):
         precios = []
         for x in divs_precios:
-            price = x.get_text(strip=True).replace('$ ', '')
-            price = price.replace('.', '')
-            price = price.replace(',', '.')
-            price = float(price)
+            price = x.get_text(strip=True).replace('$ ', '')  # Quita espacios en blanco y simbolo $
+            price = price.replace('.', '')  # Quita punto de miles
+            price = price.replace(',', '.')  # Reemplaza la coma decimal por punto
+            price = float(price)  # Convierte el numero en flotante
             precios.append(price)
         return precios
 
@@ -130,40 +129,49 @@ while page < (cant_de_paginas() + 1):
             i += 1
         return fecha
 
-    ############################## GUARDAR DATOS EN DATAFRAME ##############################
+    # GUARDAR DATOS EN DATAFRAME
     # Crear diccionario para el dataframe
-    data = dict([ 
-                ("Prcesadores", products),
-                ("Precios", precios),
-                ("Fecha", fecha)
-    ])
-    # Convertimos el diccionario en un dataframe
-    tabla = pd.DataFrame(data)
+    def crear_dataframe(products, precios, fecha):
+        data = dict([
+                    ("Prcesadores", products),
+                    ("Precios", precios),
+                    ("Fecha", fecha)
+                    ])
+        # Convertimos el diccionario en un dataframe
+        tabla = pd.DataFrame(data)
+        return tabla
 
-    ############################## AGREGAR AL ARCHIVO DEL HISTORIAL ##############################
+    # AGREGAR AL ARCHIVO DEL HISTORIAL
     # Leer archivo excel, especificando el tipo de dato de cada columna ("nombre de columna": tipo de dato),
     # en el argumento sheetname se especifica el nombre de la pestaña donde estan los datos
-    tabla_ant = pd.read_excel('Historial_de_precios.xlsx', sheet_name=0, header=0, converters={'Procesadores':str,'Precio':float, 'Fecha':str})
-    # Convertimos en dataframe los datos recuperados del archivo antiguo.
-    tabla_ant_df = pd.DataFrame(tabla_ant)
-    tabla_ant_df = tabla_ant_df.drop (tabla_ant_df.columns [[0]], axis= 1) # Borrar primer columna
-    tabla_concatenada = pd.concat([tabla_ant_df, tabla], axis=0, ignore_index= True)
+    def recuperar_tabla_anterior():
+        tabla_ant = pd.read_excel('Historial_de_precios.xlsx', sheet_name=0, header=0, converters={'Procesadores': str, 'Precio': float, 'Fecha': str})
+        # Convertimos en dataframe los datos recuperados del archivo antiguo.
+        tabla_ant_df = pd.DataFrame(tabla_ant)
+        tabla_ant_df = tabla_ant_df.drop(tabla_ant_df.columns[[0]], axis=1)  # Borrar primer columna
+        return tabla_ant_df
 
-    ############################## GUARDAR ARCHIVO ##############################
+    def concatenar_tablas_anterior_nueva(tabla_ant_df, tabla):
+        tabla_concatenada = pd.concat([tabla_ant_df, tabla], axis=0, ignore_index=True)
+        return tabla_concatenada
+
+    # GUARDAR ARCHIVO
     # Crear excel
-    writer = pd.ExcelWriter("Historial_de_precios.xlsx")
-    # Guardar dataframe a excel
-    tabla_concatenada.to_excel(writer)
-    # Cerrar
-    writer.close()
+    def guardar_excel(tabla_concatenada):
+        writer = pd.ExcelWriter("Historial_de_precios.xlsx")
+        # Guardar dataframe a excel
+        tabla_concatenada.to_excel(writer)
+        # Cerrar
+        writer.close()
 
-    ############################## PASAR DE PAGINA E INCREMENTO DEL BUCLE ##############################
+    # PASAR DE PAGINA E INCREMENTO DEL BUCLE
     # PASAR DE PAGINA ES SOLO PARA ALGO VISUAL, SU AUSENCIA NO MODIFICA EL RESULTADO
     next_page = driver.find_element(By.XPATH, value="/html/body/div[3]/div[1]/section/div/div[2]/div[2]/div[2]/div/div/li[5]/a")
-    next_page.click() 
+    next_page.click()
     # Incremento
     page += 1
 
-############################## FINAL ##############################
-driver.quit()
-print("Historial generado correctamente")
+# FINAL
+def finalizar(driver):
+    driver.quit()
+    print("Historial generado correctamente")
--- archivos de testeo/Archivos de testeos.py
+++ archivos de testeo/Archivos de testeos.py
@@ -1,40 +1,34 @@
-############################## IMPORTAR PAQUETES ##############################
-from bs4 import BeautifulSoup
-from datetime import date
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-import pandas as pd
-import requests
-import time
 
-############################## INICIO ##############################
-# Cargamos el webdriver de selenium
-service = webdriver.ChromeService(executable_path=r"C:\Users\Ivan\Documents\CODING\Python\chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-driver.get("https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52/SCAT_ID=-1/SCA_ID=-1/m=0/BUS=/compugarden.aspx")
 
-############################## AVERIGUANDO CUANTAS PAGINAS HAY EN ESTA CATEGORIA ##############################
-show_items = int(driver.find_element(By.ID, value="cant_a_mostrar").get_attribute("value")) # Cantidad de productos mostrados por pagina
-total_items_raw = driver.find_element(By.XPATH, value="/html/body/div[3]/div[1]/section/div/div[2]/div[2]/div[2]/div/p").text
-total_items_raw = ' '.join(total_items_raw.split()) # Se borran los espacios extras
-total_items = int(total_items_raw[7:9]) # Se corta la parte con la cantidad de productos y se transforma a entero
-total_pages = round(total_items / show_items)
-if total_pages < (total_items / show_items):
-  total_pages += 1
+def web_driver():
+    service = webdriver.ChromeService(executable_path=r"C:\Users\Ivan\Documents\CODING\Python\chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
+    driver.get("https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52/SCAT_ID=-1/SCA_ID=-1/m=0/BUS=/compugarden.aspx")
+    return driver
 
-############################## BUCLE DE SCRAPEO ##############################
-page = 1
-time.sleep(2)
-URL_BASE = f"https://www.compugarden.com.ar/ARTICULOS/CAT_ID=52;SCAT_ID=-1;SCA_ID=-1;m=0;BUS=;A_PAGENUMBER={page};/compugarden.aspx"
-pedido_obtenido = requests.get(URL_BASE)
-html_obtenido = pedido_obtenido.text
-# Parseamos el HTML
-soup = BeautifulSoup(html_obtenido,"html.parser")
-############################## EXTRACCION DE DATOS NUEVOS ##############################
-# Obtener y extraer datos
-divs_products = soup.find_all('div', class_='product')
-divs_precios = soup.find_all('div', class_='price')
 
-print(type(divs_precios))
+def cant_de_paginas(driver):
+    show_items = int(driver.find_element(By.ID, value="cant_a_mostrar").get_attribute("value"))  # Cantidad de productos mostrados por pagina
+    total_items_raw = driver.find_element(By.XPATH, value="/html/body/div[3]/div[1]/section/div/div[2]/div[2]/div[2]/div/p").text
+    total_items_raw = ' '.join(total_items_raw.split())  # Se borran los espacios extras
+    total_items = int(total_items_raw[7:9])  # Se corta la parte con la cantidad de productos y se transforma a entero
+    total_pages = round(total_items / show_items)
+    if total_pages < (total_items / show_items):
+        total_pages += 1
+    return total_pages
+
+
+def print_pages(total_pages):
+    print(total_pages)
+
+
+def main():
+    driver_chrome = web_driver()
+    total_paginas = cant_de_paginas(driver_chrome)
+    print_pages(total_paginas)
+
+
+if __name__ == '__main__':
+    main()
